68c842de889bcbd06aadf9ca2767287d015fe2e7
HamuRafael <82476399+HamuRafael@users.noreply.github.com>
2023-11-07 17:12:31 -0300

    terminei as telas

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+const btnHome = document.querySelector('btnHome');
+const MissPassword = document.querySelector('.miss-password');
+const loginLinkS = document.querySelector('.logins-link');
+
+MissPassword.addEventListener('click',()=>{
+    wrapper.classList.add('misspass'); 
+});
+loginLinkS.addEventListener('click',()=>{
+    wrapper.classList.remove('misspass');
+    wrapper.classList.remove('active');
+    
+});
+
+
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -13,3 +27,4 @@ btnPopup.addEventListener('click', ()=>{
     wrapper.classList.add('active-popup');
 
 });
+
