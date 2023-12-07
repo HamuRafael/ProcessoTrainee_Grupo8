2df3fedab871629e685b3d85c934fe3a36466e61
armitage001 <151403439+armitage001@users.noreply.github.com>
2023-12-07 00:04:24 -0300

    Add files via upload
    perfil

---
+++ ProcessoTrainee_Grupo8-main/ProcessoTrainee_Grupo8-main/Feed/feed-logado-script.js
@@ -0,0 +1,41 @@
+var postEditor;
+
+function closeModal(){
+    var modal=document.getElementById('postModal');
+    var overlay=document.getElementById('overlay');
+    if (modal && overlay){
+        modal.style.display='none';
+        overlay.style.display='none';
+        if (postEditor){
+            postEditor.value('');
+        }
+    }
+}
+
+function openModal(){
+    var modal = document.getElementById('postModal');
+    var overlay=document.getElementById('overlay');
+    if (modal && overlay) {
+        modal.style.display = 'block';
+        overlay.style.display='block';
+        if (!postEditor){
+            postEditor=new SimpleMDE({
+                element: document.getElementById('postEditor'),
+                placeholder: ''
+            });
+        }
+    }
+}
+
+function criarPost(){
+    var conteudoPost=postEditor.value();
+    console.log('Conteudo do post:',conteudoPost);
+    closeModal();
+}
+
+function redirectToPage(destination){
+    window.location.href=destination
+}
+
+document.getElementsByClassName('create-post').addEventListener('click',openModal);
+document.getElementById('edit').addEventListener('click',openModal);
